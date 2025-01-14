42669549107bf5eb8274bbda56401a54607251fd
Yuriy <junker098@gmail.com>
2025-01-14 13:08:26 +0200

    Initial Commit

---
+++ UniverseTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniverseTestApp
+//
+//  Created by Yuriy on 14.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
